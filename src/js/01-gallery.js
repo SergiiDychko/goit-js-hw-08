9636845dad3c7fd393d8ada804d052c842ccc562
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-15 23:12:30 +0300

    намагався пікдлючити сімплбокс. нічого не виходить

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// const SimpleLightbox = require('simplelightbox');
+import SimpleLightbox from 'simplelightbox';
+// import SimpleLightbox from '../../node_modules/simplelightbox/dist/simple-lightbox.min.js';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
+
+// Наповнюємо перелік елементів з масиву galleryItems;
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsArr = galleryItems.map(
+  ({ original, preview, description }) =>
+    `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+);
+galleryRef.insertAdjacentHTML('afterbegin', galleryItemsArr.join(''));
+
+// створюємо галерею SimpleLightbox
+new SimpleLightbox('.gallery a', {
+//  додаємо опис (alt) внизу фото 
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
 
 console.log(galleryItems);
