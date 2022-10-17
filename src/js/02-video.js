40a5aa864a737a272ca41f9285b36d00c7ec94e6
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-17 23:31:14 +0300

    відео на локалхост працює, на гітхаб обнуляється.

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,9 +4,9 @@ import throttle from 'lodash.throttle';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
 const onPlay = function (currentTime) {
   localStorage.setItem('videoplayer-current-time', currentTime.seconds);
 };
-
 player.on('timeupdate', throttle(onPlay, [wait = 1000]));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time') || 0);
